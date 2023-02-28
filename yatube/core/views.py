588d104e593bff854138854ebed72348f8dd186d
Evsey Kirichkov <evseykirichkov@mail.ru>
2023-02-28 17:11:56 +0300

    fixed for pppppeeeep8

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,9 +4,9 @@ from django.shortcuts import render
 
 def page_not_found(request, exception):
     return render(
-        request, 
-        "core/404.html", 
-        {"path": request.path}, 
+        request,
+        "core/404.html",
+        {"path": request.path},
         status=HTTPStatus.NOT_FOUND
     )
 
