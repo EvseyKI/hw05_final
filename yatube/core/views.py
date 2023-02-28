58afbd9e77a37c4e8c194dfc3ec1a0217358efd2
Evsey Kirichkov <evseykirichkov@mail.ru>
2023-02-28 17:10:16 +0300

    fixed for reviewer 6 in last sprint

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,7 +4,10 @@ from django.shortcuts import render
 
 def page_not_found(request, exception):
     return render(
-        request, "core/404.html", {"path": request.path}, status=HTTPStatus.NOT_FOUND
+        request, 
+        "core/404.html", 
+        {"path": request.path}, 
+        status=HTTPStatus.NOT_FOUND
     )
 
 
