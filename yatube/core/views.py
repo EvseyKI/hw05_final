4535f6c93fbb1e01979c7bb3dcc78b23f3ae870a
Evsey Kirichkov <evseykirichkov@mail.ru>
2023-02-28 17:07:00 +0300

    fixed for reviewer 5 in last sprint

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -3,17 +3,15 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 
-        'core/404.html', 
-        {'path': request.path}, 
-        status=HTTPStatus.NOT_FOUND
+    return render(
+        request, "core/404.html", {"path": request.path}, status=HTTPStatus.NOT_FOUND
     )
 
 
-def csrf_failure(request, reason=''):
-    return render(request, 'core/403csrf.html')
+def csrf_failure(request, reason=""):
+    return render(request, "core/403csrf.html")
 
 
 def permission_denied(request, exception):
     """Настройка шаблона для страницы с ошибкой 403."""
-    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
+    return render(request, "core/403.html", status=HTTPStatus.FORBIDDEN)
